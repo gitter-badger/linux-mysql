ea689b0e3ef3cb09393a3fe086464d3013838f68
costantino03 <30986939+costantino03@users.noreply.github.com>
2020-06-25 00:15:02 +0200

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get install python-software-properties -y
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash
+
+sudo apt-get install nodejs -y
+
+clear
+
+node -v
+
+npm -v
