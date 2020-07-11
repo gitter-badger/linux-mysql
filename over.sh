812430a3025df73add99c47cfba0e2ef7a83b88c
costantino03 <30986939+costantino03@users.noreply.github.com>
2020-07-11 19:31:11 +0200

    Create over.sh

---
+++ over.sh
@@ -0,0 +1,35 @@
+sudo wget https://raw.githubusercontent.com/costantino03/linux-mysql/master/apache2.sh
+
+sudo wget https://raw.githubusercontent.com/costantino03/linux-mysql/master/node.sh
+
+sudo wget https://raw.githubusercontent.com/costantino03/linux-mysql/master/mongo.sh
+
+sudo wget https://raw.githubusercontent.com/costantino03/linux-mysql/master/certbot.sh
+
+clear
+
+chmod 775 apache2.sh
+
+chmod 775 node.sh
+
+chmod 775 mongo.sh
+
+chmod 775 certbot.sh
+
+clear
+
+./apache2.sh
+
+clear
+
+./node.sh
+
+clear
+
+./mongo.sh
+
+clear
+
+./certbot.sh
+
+clear
