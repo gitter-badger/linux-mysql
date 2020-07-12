d4c4fa74264ff8852ad17c2e12d623339d8492b8
costantino03 <megabyteytbest@gmail.com>
2020-07-12 17:13:14 +0200

    Add files via upload

---
+++ ciao.sh
@@ -0,0 +1,75 @@
+git clone https://github.com/costantino03/we
+
+cp CONTRIBUTING.md we/CONTRIBUTING.md
+
+cp COPYRIGHT we/COPYRIGHT
+
+cp Dokerfile.example we/Dokerfile.example
+
+cp LICENSE we/LICENSE
+
+cp Makefile we/Makefile
+
+cp Procfile we/Procfile
+
+cp README.md we/README.md
+
+cp Vagrantfile we/Vagrantfile
+
+cp app.js we/app.js
+
+cp app.json we/app.json
+
+cp azuredeploy.json we/azuredeploy.json
+
+cp bin -r we/bin
+
+cp bundle -r we/bundle
+
+cp ci.test.env we/ci.env.test
+
+cp database_configuration.json -we/database_configuration.json
+
+cp deploy.sh we/deploy.sh
+
+cp docs -r we/docs
+
+cp env.js we/env.js
+
+cp example-profiles.md we/example-profiles.md
+
+cp lib -r we/lib
+
+cp my.env.template we/my.env.template
+
+cp node_modules -r we/node_modules
+
+cp npm-shrinkwrap.json we/npm-shrinkwrap.json
+
+cp package.json we/package.json
+
+cp prep_repo_relase.sh we/prep_repo_relase.sh
+
+cp server.js we/server.js
+
+cp setup.sh we/setup.sh
+
+cp static -r we/static
+
+cp swagger.json we/swagger.json
+
+cp swagger.yaml we/swagger.yaml
+
+cp testing -r we/testing
+
+cp tests -r we/tests
+
+cp tmp -r we/tmp
+
+cp update.png we/update.png
+
+cp views -r we/views
+
+cp web.config we/web.config
+
+cp webpack.config.js we/webpack.config.js
