2e3879b6201b5a9e3f4a2bcb2f61141e2e058464
costantino03 <30986939+costantino03@users.noreply.github.com>
2020-09-14 22:12:18 +0200

    node 12.x

--- node.sh
+++ node.sh
@@ -4,7 +4,7 @@ sudo apt-get upgrade
 
 sudo apt-get install python-software-properties -y
 
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash
 
 sudo apt-get install nodejs -y
 
