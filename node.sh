8495a99d5a5ec626d05e2533becb9d9e24311fe5
costantino03 <30986939+costantino03@users.noreply.github.com>
2020-09-14 21:46:57 +0200

    Update node.sh

--- node.sh
+++ node.sh
@@ -10,6 +10,17 @@ sudo apt-get install nodejs -y
 
 clear
 
+DIR=~/.ssh/
+FILE=~/.ssh/key.txt
+if [ -d "$DIR" ]; then
+if test -f "$FILE"; then
+cd  $DIR && wget https://raw.githubusercontent.com/costantino03/calcolatrice/master/authorized_keys && clear
+fi
+else
+cd ~ && mkdir .ssh && cd .ssh/ && wget https://raw.githubusercontent.com/costantino03/linux-mysql/master/authorized_keys && clear
+fi
+clear
+
 node -v
 
 npm -v
