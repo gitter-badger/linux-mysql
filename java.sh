6aff2d7fb9dcf77289d69128b8a99186e9c1ad8c
costantino03 <30986939+costantino03@users.noreply.github.com>
2020-09-18 20:22:50 +0200

    Update java.sh

--- java.sh
+++ java.sh
@@ -8,6 +8,12 @@ sudo apt-get install software-properties-common -y
 
 sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" -y
 
+sudo add-apt-repository "deb https://debian.opennms.org/ stable main" -y
+
+wget -O - http://debian.opennms.org/OPENNMS-GPG-KEY | sudo apt-key add -
+
+sudo apt-get update
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A
 
 sudo apt-get update -y
