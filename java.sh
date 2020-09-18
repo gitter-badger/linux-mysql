afc3fa05026339e033b2049cf86e13ac97fc4854
costantino03 <30986939+costantino03@users.noreply.github.com>
2020-09-18 20:15:33 +0200

    Update java.sh

--- java.sh
+++ java.sh
@@ -8,6 +8,8 @@ sudo apt-get install software-properties-common -y
 
 sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" -y
 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A
+
 sudo apt-get update -y
 
 sudo apt-get upgrade -y
