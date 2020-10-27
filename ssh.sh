8487ddfcbf4ddc612127cca0e3464608c3f0171b
costantino03 <30986939+costantino03@users.noreply.github.com>
2020-10-27 22:07:37 +0100

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -6,11 +6,11 @@ sudo apt-get purge openssh-server -y
 
 sudo apt-get install openssh-server -y
 
-sudo rm /etc/ssh/sshd_config
+#sudo rm /etc/ssh/sshd_config
 
 sudo apt-get install wget -y
 
-sudo cd /etc/ssh
+#sudo cd /etc/ssh
 
 #wget https://raw.githubusercontent.com/costantino03/linux-mysql/master/sshd_config
 
